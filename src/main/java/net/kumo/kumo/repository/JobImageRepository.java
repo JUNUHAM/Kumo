776be1fb27f4952fe781761387fcec65a01f26cc
YoonByungjin <dbsqudwls77@gmail.com>
2026-02-09 16:27:49 +0900

    2차 커밋

--- src/main/java/net/kumo/kumo/repository/JobImageRepository.java
+++
@@ -1,9 +0,0 @@
-package net.kumo.kumo.repository;
-
-import net.kumo.kumo.domain.entity.JobImageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface JobImageRepository extends JpaRepository<JobImageEntity, Long> {
-}
--- src/main/java/net/kumo/kumo/repository/OsakaGeocodedRepository.java
+++
@@ -1,9 +0,0 @@
-package net.kumo.kumo.repository;
-
-import net.kumo.kumo.domain.entity.OsakaGeocodedEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface OsakaGeocodedRepository extends JpaRepository<OsakaGeocodedEntity, Long> {
-}
