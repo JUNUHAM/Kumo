3a8a4522fde6babb2ee051716828927367a02ffc
wanudesu <wanudesu@gmail.com>
2026-02-09 16:34:16 +0900

    merge to main 전 파일정리

--- src/main/java/net/kumo/kumo/repository/TokyoNoGeocodedRepository.java
+++
@@ -1,8 +0,0 @@
-package net.kumo.kumo.repository;
-
-import net.kumo.kumo.domain.entity.TokyoNoGeocodedEntity;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TokyoNoGeocodedRepository extends BaseRepository<TokyoNoGeocodedEntity> {
-}
