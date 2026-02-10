851a8259d8ab7170ce0b7f16b1b587f8d800930b
wanudesu <wanudesu@gmail.com>
2026-02-10 18:06:07 +0900

    구인자 페이지 다크모드 & 번역기능 완성

--- src/main/java/net/kumo/kumo/controller/MapController.java
+++ src/main/java/net/kumo/kumo/controller/MapController.java
@@ -1,19 +1,20 @@
 package net.kumo.kumo.controller;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import net.kumo.kumo.domain.dto.JobDetailDTO;
-import net.kumo.kumo.domain.dto.JobSummaryDTO;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import net.kumo.kumo.service.MapService;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.kumo.kumo.domain.dto.JobDetailDTO;
+import net.kumo.kumo.domain.dto.JobSummaryDTO;
+import net.kumo.kumo.service.MapService;
 
 @Slf4j
 @Controller
@@ -21,24 +22,25 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MapController {
 
-	// Key
-	@Value("${GOOGLE_MAPS_KEY}")
-	private String googleMapKey;
-	
+    // Key
+    @Value("${kumo.google.maps.keys}")
+    private String googleMapKey;
+
     private final MapService mapService;
 
+    /**
+     * 지도 메인페이지 연결
+     * 
+     * @return 메인 페이지
+     */
+    @GetMapping("main")
+    public String mainMap(Model model) {
+        log.debug("메인화면 연결");
 
-	/**
-	 * 지도 메인페이지 연결
-	 * @return 메인 페이지
-	 */
-	@GetMapping("main")
-	public String mainMap(Model model) {
-		log.debug("메인화면 연결");
+        model.addAttribute("googleMapsKey", googleMapKey);
+        return "mainView/main";
+    }
 
-		model.addAttribute("googleMapsKey", googleMapKey);
-		return "mainView/main";
-	}
     /**
      * [VIEW] 구인 리스트 페이지 반환
      * 파일 위치: resources/templates/mapView/job_list.html
@@ -50,12 +52,13 @@ public class MapController {
 
     /**
      * 공고 상세 페이지 이동
-     * @param id        공고 아이디
-     * @param source    지역 꼬리표 'OSAKA', 'TOKYO' 등
-     * @param lang      언어 설정 'kr', 'jp'
-     * @param isOwner   공고 작성자 여부 (임시 테스트용, 추후 로그인 기능 구현시 Authenticate 로 변경)
+     * 
+     * @param id      공고 아이디
+     * @param source  지역 꼬리표 'OSAKA', 'TOKYO' 등
+     * @param lang    언어 설정 'kr', 'jp'
+     * @param isOwner 공고 작성자 여부 (임시 테스트용, 추후 로그인 기능 구현시 Authenticate 로 변경)
      * @param model
-     * @return          mapView/job_detail.html 로 이동
+     * @return mapView/job_detail.html 로 이동
      */
     @GetMapping("/jobs/detail")
     public String jobDetailPage(
@@ -66,8 +69,7 @@ public class MapController {
             // 기본값은 false (구직자 모드)
             @RequestParam(defaultValue = "false") boolean isOwner,
 
-            Model model
-    ) {
+            Model model) {
         // 1. 서비스에서 상세 데이터 조회
         JobDetailDTO job = mapService.getJobDetail(id, source, lang);
 
@@ -91,8 +93,7 @@ public class MapController {
             @RequestParam Double maxLat,
             @RequestParam Double minLng,
             @RequestParam Double maxLng,
-            @RequestParam(defaultValue = "kr") String lang
-    ) {
+            @RequestParam(defaultValue = "kr") String lang) {
         // 서비스가 이미 정제된(JobResponse) 데이터를 줍니다.
         return mapService.getJobListInMap(minLat, maxLat, minLng, maxLng, lang);
     }
