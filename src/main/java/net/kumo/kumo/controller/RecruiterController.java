0ccae78fc829ed9386fa8dc4a3b376e78332fb8c
Yeon72 <rhe3045@gmail.com>
2026-02-26 09:46:08 +0900

    지원자 정보 db랑 연결(머지 후 정상작동)

--- src/main/java/net/kumo/kumo/controller/RecruiterController.java
+++ src/main/java/net/kumo/kumo/controller/RecruiterController.java
@@ -66,13 +66,23 @@ public class RecruiterController {
 
     /**
      * 지원자 관리 컨트롤러
-     * 
-     * @param model
-     * @return
      */
     @GetMapping("ApplicantInfo")
-    public String ApplicantInfo(Model model) {
-        model.addAttribute("currentMenu", "applicants"); // 사이드바 선택((지원자 관리)
+    public String ApplicantInfo(Model model, Principal principal) {
+        model.addAttribute("currentMenu", "applicants"); // 사이드바 선택
+
+        // 1. 현재 로그인한 구인자 정보 가져오기
+        String loginEmail = principal.getName();
+        UserEntity user = ur.findByEmail(loginEmail)
+                .orElseThrow(() -> new RuntimeException("사용자 정보를 찾을 수 없습니다."));
+
+        // 2. 서비스 호출: 이 구인자가 올린 공고에 지원한 모든 지원자 목록 가져오기
+        // 머지 후 구현 가능!!
+        //List<ApplicationDTO.ApplicantResponse> applicantList = rs.getApplicantsForRecruiter(user.getUserId());
+
+        // 3. 화면(Model)에 데이터 전달
+        //model.addAttribute("applicantList", applicantList);
+
         return "recruiterView/applicantInfo";
     }
 
